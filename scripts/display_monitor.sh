fc47b1b974e8ca73117204478cb24b5a4dd9bef6
WK <1472411+quantumxt@users.noreply.github.com>
2024-06-12 14:56:47 +0800

    fix: Update output config

--- scripts/display_monitor.sh
+++ scripts/display_monitor.sh
@@ -5,6 +5,12 @@ TARGET_RES="1920x1080"
 # Set Resolution
 xrandr --output $MONITOR_MAIN --mode $TARGET_RES
 
-if xrandr | grep -q '$MONITOR_EXT connected' ; then
+check=$(xrandr | grep "$MONITOR_EXT connected" | wc -l)
+notify-send "[Debug] $MONITOR_EXT Connected: $check"
+if [ $check -eq 1 ] ; then
+	notify-send "External display detected!"
     xrandr --output $MONITOR_EXT --auto --right-of $MONITOR_MAIN
+else
+	xrandr --output $MONITOR_EXT --off
+	notify-send "External display not connected!"
 fi
